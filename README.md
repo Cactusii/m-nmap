119f3b1c548862c1fe97dcea31b8b6829ce9cc01
lmx <lmx4098@163.com>
2022-03-03 16:29:04 +0800

    update

---
+++ README.md
@@ -0,0 +1,13 @@
+Nmap原地址：https://github.com/nmap/nmap
+
+Installing
+----------
+
+```bash
+# for centos:
+yum install git gcc-c++ autoconf automake -y
+bash configure
+make
+chmod 777 shtool
+make install
+```
---
+++ libz/out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/lmx40_qjy9utl/Documents/Cproj/vs/nmap-master/libz
+# Build directory: C:/Users/lmx40_qjy9utl/Documents/Cproj/vs/nmap-master/libz/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
+set_tests_properties(example PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/lmx40_qjy9utl/Documents/Cproj/vs/nmap-master/libz/CMakeLists.txt;235;add_test;C:/Users/lmx40_qjy9utl/Documents/Cproj/vs/nmap-master/libz/CMakeLists.txt;0;")
